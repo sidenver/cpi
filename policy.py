8bc50c17d806021b524e0da536b8a4b60d032353
sidenver <hanchinshing@gmail.com>
2016-11-22 12:26:56 -0500

    add conservative update

--- policy.py
+++ policy.py
@@ -1,9 +1,10 @@
 
-
 class Policy(object):
-
     def __init__(self):
         pass
 
     def getAction(self, state):
+        pass
+
+    def conservativeUpdate(self, alpha, policy):
         pass
