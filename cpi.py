938c78372a8819700f6ceed90b6cc46e063c059f
sidenver <hanchinshing@gmail.com>
2016-12-06 22:37:33 -0500

    lint error

--- cpi.py
+++ cpi.py
@@ -1,19 +1,20 @@
 from game import game
 from greedyPolicy import GreedyPolicy
 from advantage import AdvantageEstimator
+from policy import Policy
 
 if __name__ == '__main__':
     env = game()
-    dist = 0 # restart distribution
-    discount = 0.5 # discount factor
-    iteration = 1000 # number of iteration of learning
+    dist = 0  # restart distribution
+    discount = 0.5  # discount factor
+    iteration = 1000  # number of iteration of learning
     # TODO accuracy?
-    accuracy = 0.9 # accuracy
-    sampleSize = 100 # number of sample's states
-    horizon = 1000 # finite horizon
+    accuracy = 0.9  # accuracy
+    sampleSize = 100  # number of sample's states
+    horizon = 1000  # finite horizon
     greedyChooser = GreedyPolicy(env)
     # TODO initalize policy
-    policy = greedyChooser.getGreedyPolicy([])  # initalized here
+    policy = Policy(env)  # initalized here
     advantageEstimator = AdvantageEstimator(env, dist, discount)
     for k in range(iteration):
         estimate = advantageEstimator.estimateAdvantage(greedyChooser, policy, sampleSize, horizon)
